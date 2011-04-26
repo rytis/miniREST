aba671b7f15b277d5fda1b64fd9c0fdc0808e505
Rytis Sileika <rytis.sileika@gmail.com>
2011-04-26 09:23:22 +0100

    Updated setup.py with new version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='miniREST',
-      version='0.1',
+      version='0.2',
       description='Minimalistic REST client library',
       author='Rytis Sileika',
       author_email='rytis.sileika@gmail.com',
